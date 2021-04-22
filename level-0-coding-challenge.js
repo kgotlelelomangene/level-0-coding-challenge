218a8559f5d87b1ef7724f0305b4ac0a630b8924
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 16:25:36 +0200

    task 0.9 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -117,3 +117,16 @@ function hoursAndMinutes(num){
 }
 
 hoursAndMinutes(71);
+
+//Task 0.9
+console.log("\nTask 0.9 ");
+
+function vowels(str){
+
+    var vowel = str.match(/[aeiou]/gi);
+    vowel.forEach(x => {console.log(x) });;
+
+}
+
+vowels("We are here to learn");
+
