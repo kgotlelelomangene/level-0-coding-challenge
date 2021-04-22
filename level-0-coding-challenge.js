70a830f15df6a7f9d5a13626fafba17d4342dab4
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 16:24:14 +0200

    task 0.8 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -105,3 +105,15 @@ function fahrenheitToCelsius(num){
 
 }
 console.log(fahrenheitToCelsius(50));
+
+//Task 0.8
+console.log("\nTask 0.8 ");
+
+function hoursAndMinutes(num){
+    var hours = Math.floor(num/60);
+    var minutes = Math.floor(num%60);
+
+    console.log(num +  " has " + hours + " hour(s), " + minutes + " minute(s)");
+}
+
+hoursAndMinutes(71);
