7bd0c713f83c95418d85b39dd23736c8314f1e38
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 15:46:03 +0200

    task 0.1 commit

---
+++ level-0-coding-challenge.js
@@ -0,0 +1,14 @@
+//Task 0.1
+console.log("Task 0.1");
+
+var x = 0;
+var y = 1;
+
+console.log("The value of x : " + x);
+console.log("The value of y : " + y);
+
+x = x + 3;
+y = y + x;
+
+console.log("The value of x : " + x);
+console.log("The value of y : " + y);
