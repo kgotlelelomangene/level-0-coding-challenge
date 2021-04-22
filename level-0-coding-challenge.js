f9e48d64baf5e15a44fd14a589e54025550bcb71
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 15:47:53 +0200

    task 0.2 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -12,3 +12,18 @@ y = y + x;
 
 console.log("The value of x : " + x);
 console.log("The value of y : " + y);
+
+//Task 0.2
+console.log("\nTask 0.2 ");
+
+var x = 1 + 1 * 2;
+var y = (1 + 1) * 2;
+var z = 1 + ( 1 * 2 );
+var a = 1 + 1 * 2 / 2;
+var b = (1 + 1 * 2 ) /  2;
+
+console.log("The value of x : " + x);
+console.log("The value of y : " + y);
+console.log("The value of z : " + z);
+console.log("The value of a : " + a);
+console.log("The value of b : " + b);
