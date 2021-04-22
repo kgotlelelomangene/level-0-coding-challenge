0e568b6b670b2313e5bae84b28173bcaf4fcab02
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 16:28:07 +0200

    task 0.10 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -130,3 +130,29 @@ function vowels(str){
 
 vowels("We are here to learn");
 
+
+//Task 0.10
+console.log("\nTask 0.10 ");
+
+function commonCharacters(str1, str2){
+
+    var common = "";
+
+    var arr1 = str1.split("");
+    var arr2 = str2.split("");
+
+    for (let i = 0; i < arr1.length; i++) {
+        var index = arr2.indexOf(arr1[i]);
+        if (index !== -1) {
+            arr2.splice(index, 1);
+             
+            common = common + " " + arr1[i];
+            
+        }
+    }
+
+    return common;
+
+}
+
+console.log("Common letters: " + commonCharacters("house","computers"));
