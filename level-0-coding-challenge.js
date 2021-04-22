afde670c7c7e6e7ff63229cddcd14e3067b717d4
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 15:58:47 +0200

    task 0.6 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -66,3 +66,21 @@ function areaOfATriangle(sideA ,sideB ,sideC ){
     return area;
 }
 console.log(areaOfATriangle(10,12,15));
+
+//Task 0.6
+console.log("\nTask 0.6 ");
+
+function maximumNum(num1, num2, num3){
+
+    if(num1 > num2 && num1 > num3){
+        return num1;
+    }
+    else if(num2 > num1 && num2 > num3){
+        return num2;
+    }
+    else if(num3 > num1 && num3 > num2){
+        return num3;
+    }
+}
+
+console.log(maximumNum(9,72,8) + " is the maximum number");
