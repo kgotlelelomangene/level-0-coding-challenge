d738a4cde53320d906c241feb9966a1f17487b42
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 16:17:23 +0200

    task 0.7 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -84,3 +84,24 @@ function maximumNum(num1, num2, num3){
 }
 
 console.log(maximumNum(9,72,8) + " is the maximum number");
+
+//Task 0.7
+console.log("\nTask 0.7 ");
+
+function celsiusToFahrenheit(num){
+  var fahrenheit = (num * (9/5)) + 32;
+
+  return fahrenheit;
+
+}
+
+console.log(celsiusToFahrenheit(10));
+
+
+function fahrenheitToCelsius(num){
+    var celsius = (num - 32) * (5/9);
+
+  return celsius;
+
+}
+console.log(fahrenheitToCelsius(50));
