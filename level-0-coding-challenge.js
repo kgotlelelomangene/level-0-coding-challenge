b24e6289e78d89b578b040e8cc27bf1779600ea0
kgotlelelomangene <kgotlelelomangene@gmail.com>
2021-04-22 15:51:23 +0200

    task 0.4 commit

--- level-0-coding-challenge.js
+++ level-0-coding-challenge.js
@@ -36,3 +36,18 @@ function hello(str){
 }
 
 hello("Kgotlelelo");
+
+//Task 0.4
+console.log("\nTask 0.4 ");
+
+function evenOrOdd(int){
+    if(int%2 == 0 ){
+        console.log("Even");
+    }
+    else{
+        console.log("Odd");
+    }
+        
+}
+
+evenOrOdd(12);
